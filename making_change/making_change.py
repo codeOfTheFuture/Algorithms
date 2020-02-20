f95d46e052fc476a7f6e7c387a583f030f8c2d5d
Jeff Oliver <jeffoliver3384@gmail.com>
2020-02-20 00:14:48 -0500

    completed stock prices

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -2,16 +2,28 @@
 
 import sys
 
+
+
 def making_change(amount, denominations):
-  pass 
+  cache = [0 for i in range(0, amount + 1)]
+  print(cache)
+  cache[0] = 1
+  for i in range(0, len(denominations)):
+    for higher_amount in range(cache[0], amount + 1):
+      difference = higher_amount - i
+      cache[higher_amount] = difference
+  return cache[len(cache) -2]
 
+amount = 10
+denominations = [1, 5, 10, 25, 50]
+print(making_change(amount, denominations))
 
-if __name__ == "__main__":
-  # Test our your implementation from the command line
-  # with `python making_change.py [amount]` with different amounts
-  if len(sys.argv) > 1:
-    denominations = [1, 5, 10, 25, 50]
-    amount = int(sys.argv[1])
-    print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
-  else:
-    print("Usage: making_change.py [amount]")
+# if __name__ == "__main__":
+#   # Test our your implementation from the command line
+#   # with `python making_change.py [amount]` with different amounts
+#   if len(sys.argv) > 1:
+#     denominations = [1, 5, 10, 25, 50]
+#     amount = int(sys.argv[1])
+#     print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
+#   else:
+#     print("Usage: making_change.py [amount]")
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,7 +3,22 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  current_min_price_so_far = prices[0]
+  max_profit_so_far = 0
+
+  for i in range(1, len(prices)):
+    if max_profit_so_far > 0:
+      if prices[i] - current_min_price_so_far > max_profit_so_far:
+        max_profit_so_far = prices[i] - current_min_price_so_far
+    else:
+      if prices[i] < current_min_price_so_far:
+        max_profit_so_far = prices[i] - current_min_price_so_far
+        current_min_price_so_far = prices[i]
+      else:
+        max_profit_so_far = prices[i] - current_min_price_so_far
+  return max_profit_so_far
+
+find_max_profit([100, 90, 80, 50, 20, 10])
 
 
 if __name__ == '__main__':
