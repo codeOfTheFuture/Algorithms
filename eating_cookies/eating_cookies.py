49a994cca68c0ee97d4d13816dd33a51dce171b3
Jeff Oliver <jeffoliver3384@gmail.com>
2020-03-20 02:12:06 -0400

    fixed eating cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -7,23 +7,24 @@ import sys
 # recursive solution
 def eating_cookies(n, cache=None):
   if cache is None:
-      cache = {}
-  if n < 0:
-    return 
-  elif n == 0:
+    cache = {}
+  if n == 0 or n == 1:
     return 1
-  elif cache and n in cache:
+  elif n == 2:
+    return 2
+  elif n == 3:
+    return 4
+  elif cache and cache[n]:
     return cache[n]
-  
-  value = eating_cookies(n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3)
-  cache[n] = value
-  return value
-
-
-
-if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_cookies = int(sys.argv[1])
-    print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
   else:
-    print('Usage: eating_cookies.py [num_cookies]')
+    cache[n] = eating_cookies(n - 1, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 3, cache)
+  return cache[n]
+
+print(eating_cookies(400, {}))
+
+# if __name__ == "__main__":
+#   if len(sys.argv) > 1:
+#     num_cookies = int(sys.argv[1])
+#     print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
+#   else:
+#     print('Usage: eating_cookies.py [num_cookies]')
